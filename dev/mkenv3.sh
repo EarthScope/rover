2aa2ce94f167395f8541cbf121122cfa6652986f
andrew cooke <andrew@acooke.org>
2018-04-19 12:20:21 -0300

    more arg parsing

--- dev/mkenv3.sh
+++ dev/mkenv3.sh
@@ -2,5 +2,7 @@
 
 rm -fr env
 virtualenv-3.4 env
+source env/bin/activate
+pip install nose
 
 echo "source env/bin/activate"
--- rover/params.py
+++ rover/params.py
@@ -4,7 +4,28 @@ from os.path import exists, expanduser
 import sys
 
 
+NO = '--no-'
+DAEMON = 'daemon'
+F, FILE = 'f', 'file'
+HELP = 'help'
+
+
+def parse_bool(value):
+    '''
+    Treat caseless true, yes and on and true, anthing else as false.
+    '''
+    if not value:
+        value = 'False'
+    value = value.lower()
+    return value in ('true', 'yes', 'on')
+
+
 class StoreBoolAction(Action):
+
+    '''
+    We need a special action for booleans because we must covertly
+    parse them as '--foo True' even though the user only types '--foo'.
+    '''
 
     def __init__(self,
                  option_strings,
@@ -19,7 +40,7 @@ class StoreBoolAction(Action):
             nargs=1,
             const=None,
             default=default,
-            type=bool,
+            type=parse_bool,
             choices=choices,
             required=required,
             help=help,
@@ -27,6 +48,11 @@ class StoreBoolAction(Action):
 
     def __call__(self, parser, namespace, values, option_string=None):
         setattr(namespace, self.dest, values)
+
+
+
+def m(string): return '-' + string
+def mm(string): return '--' + string
 
 
 class RoverArgumentParser(ArgumentParser):
@@ -48,11 +74,12 @@ class RoverArgumentParser(ArgumentParser):
 
 
     def __init__(self):
-        super().__init__(description='ROVER: Retrieval of Various Experiment data Robustly')
+        super().__init__(fromfile_prefix_chars='@',
+                         description='ROVER: Retrieval of Various Experiment data Robustly')
         self.register('action', 'store_bool', StoreBoolAction)
         # TODO - windows needs different path
-        self.add_argument('-f', '--file', default="~/.rover", help='specify configuration file')
-        self.add_argument('--daemon', default=False, action='store_bool', help='use background daemons')
+        self.add_argument(m(F), mm(FILE), default="~/.rover", help='specify configuration file')
+        self.add_argument(mm(DAEMON), default=False, action='store_bool', help='use background daemons')
 
     def parse_args(self, args=None, namespace=None):
         '''
@@ -70,21 +97,33 @@ class RoverArgumentParser(ArgumentParser):
         return super().parse_args(args=args, namespace=namespace)
 
     def preprocess_booleans(self, args):
-        # find any booleans
+        '''
+        Replace --foo with '--foo True' and --no-foo with '--foo False'.
+        This makes the interface consistent with the config file (which has
+        the format 'foo=True') while letting the user trype simple flags.
+        '''
         indices = []
         for (index, arg) in enumerate(args):
-            if arg.startswith('--no-'):
+            if arg.startswith(NO):
                 arg = '--' + arg[5:]
             for action in self._actions:
                 if '--' + action.dest == arg and type(action) is StoreBoolAction:
                     indices.append(index)
-        # insert literal values for command line flags
         for index in reversed(indices):
-            negative = args[index].startswith('--no-')
+            negative = args[index].startswith(NO)
+            if negative:
+                args[index] = mm(args[index][5:])
             args.insert(index+1, str(not negative))
+        print(args)
         return args
 
     def extract_config(self, args):
+        '''
+        Find the config file, if given, otherwise use the default.
+        This must be done before arrgument parsing because we need
+        to add the contents of the file to the arguments (that is
+        how the file is read).
+        '''
         config, indices = None, []
         # find all occurences of file params, saving last
         for (index, arg) in enumerate(args):
@@ -98,21 +137,43 @@ class RoverArgumentParser(ArgumentParser):
         for index in reversed(indices):
             args = args[:index] + args[index+2:]
         if not config:
-            config = self.get_default('file')
-        return config, args
+            config = self.get_default(FILE)\
+        # include config flag so that it is set correctly, even if the extracted
+        # value is th eone that is used here
+        return config, [mm(FILE), config] + args
 
     def generate_default_config(self, config):
+        '''
+        If the config file is missing, fill it with default values.
+        '''
         config = expanduser(config)
         if not exists(config):
             with open(config, 'w') as out:
                 for action in self._actions:
-                    if action.dest not in ('help', 'file'):
-                        if action.default:
+                    print(action)
+                    if action.dest not in (HELP, FILE):
+                        if action.default is not None:
                             if action.help:
                                 out.write('# %s\n' % action.help)
                             out.write('%s=%s\n' % (action.dest, action.default))
         return
 
     def patch_config(self, args, config):
-        return args
+        '''
+        Force the reading of the config file.
+        '''
+        return ['@'+config] + args
 
+    def convert_arg_line_to_args(self, arg_line):
+        '''
+        Parse the config file, constructing '--name value" from name=value
+        and ignoring comments.
+        '''
+        arg_line = arg_line.strip()
+        if arg_line.startswith('#'):
+            return []
+        elif '=' in arg_line:
+            name, value = arg_line.split('=', 1)
+            return [mm(name), value]
+        else:
+            raise Exception('Cannot parse "%s"' % arg_line)
