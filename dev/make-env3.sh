ace057a6dd1165fe9646ee026718c4885ba2d651
andrew cooke <andrew@acooke.org>
2018-04-20 16:00:37 -0300

    rename env

---
+++ dev/make-env3.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+rm -fr env3
+virtualenv-3.4 env3
+source env3/bin/activate
+pip install nose
+pip install future
+
+echo "source env3/bin/activate"
--- dev/mkenv3.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-rm -fr env
-virtualenv-3.4 env
-source env/bin/activate
-pip install nose
-
-echo "source env/bin/activate"
