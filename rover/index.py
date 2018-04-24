2c18ec4eb8efddf7562fbe566e5b5570d43b002c
andrew cooke <andrew@acooke.org>
2018-04-24 08:40:05 -0300

    more mseedindex work

--- rover/index.py
+++ rover/index.py
@@ -1,6 +1,8 @@
 
 from contextlib import closing
-from os.path import join, basename
+
+from os import listdir, stat
+from os.path import join, basename, expanduser, abspath, isfile, isdir, dirname
 
 from .sqlite import Sqlite, NoResult
 
@@ -12,7 +14,7 @@ class Workers():
 
 class Mseedindex(Sqlite):
     '''
-    Wrap the mseedindex, extending functionality to:
+    Wrap mseedindex, extending functionality to:
     * Work with a standard directory layout on disk
     * Run multiple instances in parallel.
     * Skip files aalready processed (using size and last modified date)
@@ -24,40 +26,45 @@ class Mseedindex(Sqlite):
 
     def __init__(self, dbpath, root, log):
         super().__init__(dbpath, log)
-        self._root = root
         self._load_tables()
-        self._check_root()
+        self._check_root(abspath(expanduser(root)))
 
     def _load_tables(self):
         self._execute('''create table if not exists rover_mseeddirs (
                            id integer primary key autoincrement,
                            parent integer,
-                           path text not null,
-                           foreign key (parent) references rover_mseeddirs(id)
+                           path text unique,
+                           foreign key (parent) references rover_mseeddirs(id) on delete cascade
                      )''')
         self._execute('''create table if not exists rover_mseedfiles (
                             id integer primary key autoincrement,
                             dir integer not null,
+                            name text not null,
                             size integer not null,
                             last_modified integer not null,
-                            foreign key (dir) references rover_mseeddirs(id)
+                            foreign key (dir) references rover_mseeddirs(id) on delete cascade,
+                            constraint dir_name unique on rover_mseedfiles(dir, name)
                      )''')
 
-    def _check_root(self):
+    def _check_root(self, root):
         c = self._db.cursor()
         try:
-            path = self._fetchsingle('select path from rover_mseeddirs where parent is null')
-            if self._root != path:
-                self._log.warn('Index root has changed ("%s" != "%s")' % (self._root, path))
+            path = self._root()
+            if root != path:
+                self._log.warn('Index root has changed ("%s" != "%s")' % (root, path))
                 self._remove_all_dirs()
-                self._add_dir(None, self._root)
+                self._add_dir(None, root)
         except NoResult:
-            self._add_dir(None, self._root)
+            self._add_dir(None, root)
+
+    def _root(self):
+        return self._fetchsingle('select path from rover_mseeddirs where parent is null')
 
     def scan(self):
-        self._scan_dir(self._root, 0)
+        self._scan_dir(self._root(), 0)
 
     def _scan_dir(self, dir, level):
+        self._log.debug('Scanning directory "%s" (level %d / 4)' % (dir, level))
         if level == 4:
             self._process_files(dir)
         else:
@@ -74,8 +81,8 @@ class Mseedindex(Sqlite):
                     self._remove_dir(join(dir, db_dirs.pop()))
                 else:
                     # database missing entry on file system
-                    self._add_dir(join(dir, dir, fs_dirs[-1]))
-                    db_dirs.push(fs_dirs[-1])
+                    self._add_dir(dir, join(dir, fs_dirs[-1]))
+                    db_dirs.append(fs_dirs[-1])
 
     def _process_files(self, dir):
         db_files = [' '] + self._db_files(dir)
@@ -95,41 +102,60 @@ class Mseedindex(Sqlite):
                 file = join(dir, fs_files.pop())
                 self._scan_and_record_file(file)
 
+    def _id_for_dir(self, dir):
+        return self._fetchsingle('select id from rover_mseeddirs where path like ?', (dir,))
+
     def _db_dirs(self, dir):
-        parent = self._fetchsingle('select parent from rover_mseeddirs where path like ?', (dir,))
+        parent = self._id_for_dir(dir)
         paths = self._fetchall('select path from rover_mseeddirs where parent = ?', (parent,))
         dirs = [basename(path) for path in paths]
         return sorted(dirs)
 
     def _fs_dirs(self, dir):
-        pass
+        return sorted([name for name in listdir(dir) if isdir(join(dir, name))])
 
     def _remove_dir(self, dir):
-        pass
+        id = self._id_for_dir(dir)
+        # this uses cascade to delete files and child dirs
+        self._execute('delete from rover_mseeddirs  where id = ?' (id,))
+        # todo - delete mseedindex too
 
     def _remove_all_dirs(self):
         self._log.warn('Deleting all entries from index')
-        self._execute('delete from rover_mseedfiles')
+        # this uses cascade to delete files
         self._execute('delete from rover_mseeddirs')
-        # todo - delete mseedindex too?
+        # todo - delete mseedindex too
 
     def _add_dir(self, parent, dir):
-        pass
+        if parent:
+            id = self._id_for_dir(parent)
+        else:
+            id = None
+        self._execute('insert into rover_mseeddirs (parent, path) values (?, ?)', (id, dir))
 
     def _db_files(self, dir):
-        pass
+        id = self._id_for_dir(dir)
+        results = self._fetchall('select name from rover_mseedfiles where dir = ?', (id,))
+        return sorted([result[0] for result in results])
 
     def _fs_files(self, dir):
-        pass
+        return sorted([name for name in listdir(dir) if isfile(join(dir, name))])
 
     def _modified(self, file):
-        pass
+        statinfo = stat(file)
+        id = self._id_for_dir(dirname(file))
+        lastmod, size = self._fetchone('select last_modified, size from rover_mseedfiles where dir = ? and name = ?',
+                                       (id, basename(file)))
+        self._log.debug('%s lastmod %d / %d; size %d / %d' % (lastmod, statinfo.st_atime, size, statinfo.st_size))
+        return lastmod != statinfo.st_atime or size != statinfo.st_size
 
     def _scan_and_record_file(self, file):
         pass
 
     def _remove_file(self, file):
-        pass
+        id = self._id_for_dir(dirname(file))
+        self._execute('delete from rover_mseedfiles where dir = ? and name = ?', (id, basename(file)))
+        # todo - delete mseedindex too
 
     def close(self):
         # todo - wait for workers
--- rover/sqlite.py
+++ rover/sqlite.py
@@ -1,7 +1,11 @@
 from sqlite3 import connect
 
 
-class NoResult(Exception): pass
+class NoResult(Exception):
+
+    def __init__(self, sql, params):
+        super().__init__('%s %s' % (sql, params))
+
 
 
 class Sqlite():
@@ -12,6 +16,8 @@ class Sqlite():
     def __init__(self, dbpath, log):
         self._log = log
         self._db = connect(dbpath)
+        # https://www.sqlite.org/foreignkeys.html
+        self._execute('PRAGMA foreign_keys = ON')
 
     def _execute(self, sql, params=tuple()):
         c = self._db.cursor()
@@ -27,7 +33,15 @@ class Sqlite():
             else:
                 return result[0]
         else:
-            raise NoResult(sql)
+            raise NoResult(sql, params)
+
+    def _fetchone(self, sql, params=tuple()):
+        c = self._db.cursor();
+        result = c.execute(sql, params).fetchone()
+        if result:
+            return result
+        else:
+            raise NoResult(sql, params)
 
     def _fetchall(self, sql, params=tuple()):
         c = self._db.cursor();
