d85efeab0d5e73c76aa7ef01ce4c9884e2509146
andrew cooke <andrew@acooke.org>
2018-07-23 15:53:12 -0400

    remove need for gtar

--- dev/make-py23-tarball.sh
+++ dev/make-py23-tarball.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
+echo "WARNING: this will delete any build directory in roverdev"
+echo "(as well as replacing rover23)"
+
 dev/translate-py3-to-py23.sh
 
-pushd ../rover23
-
-rm -fr tests
 pushd ..
-tar cvfz rover.tgz --transform s/rover23/rover/ rover23
+rm -fr build
+mkdir build
+cp -r rover23 build/rover
+
+pushd build/rover
+rm -fr tests
+
+pushd ..
+tar cvfz rover.tgz rover
 popd
+
+popd
+mv build/rover.tgz .
+rm -fr build
 
 popd
