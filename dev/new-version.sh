10687f6115fdd581456565a9ae69bb2488300b84
andrew cooke <andrew@acooke.org>
2018-07-31 16:11:00 -0400

    update version in setup.py

--- dev/new-version.sh
+++ dev/new-version.sh
@@ -6,18 +6,23 @@ if [ "$#" -ne 1 ]; then
 fi
 
 echo
-echo -n "previous version: "
+echo -n "previous version (python): "
 egrep "^ROVER_VERSION.*" rover/args.py | sed -e 's/ROVER_VERSION = //' | sed -e "s/'//g"
+echo -n "previous version (setup.py): "
+egrep "version=.*" setup.py | sed -e 's/version=//' | sed -e 's/[" ,]//g'
 
 VERSION="$1"
 echo
 echo "new version: $VERSION"
 
 sed -i -e "s/^ROVER_VERSION.*/ROVER_VERSION = '$VERSION'/" rover/args.py
+sed -i -e "s/version=.*/version=\"$VERSION\",/" setup.py
 
 echo
-echo -n "updated version: "
+echo -n "updated version (python): "
 egrep "^ROVER_VERSION.*" rover/args.py | sed -e 's/ROVER_VERSION = //' | sed -e "s/'//g"
+echo -n "updated version (setup.py): "
+egrep "version=.*" setup.py | sed -e 's/version=//' | sed -e 's/[" ,]//g'
 
 echo
 echo "building tarball"
@@ -30,7 +35,7 @@ popd > /dev/null
 
 echo
 echo "tagging version in git"
-git tag -a v$VERSION -m "version $VERSION"
+#git tag -a v$VERSION -m "version $VERSION"
 echo
 echo "to share the tag:"
 echo "  git push origin --tags"
