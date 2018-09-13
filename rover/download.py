c6890ba30867e3fe448be0dc3d3280e88698b0c5
andrew cooke <andrew@acooke.org>
2018-09-13 15:55:02 -0300

    extend download to POST files

--- rover/download.py
+++ rover/download.py
@@ -4,13 +4,12 @@ from os.path import join, exists
 
 from .args import DOWNLOAD, TEMPDIR, DELETEFILES, INGEST, \
     TEMPEXPIRE, HTTPTIMEOUT, \
-    HTTPRETRIES
+    HTTPRETRIES, DATASELECTURL
 from .ingest import Ingester
 from .sqlite import SqliteSupport
 from .utils import uniqueish, get_to_file, unique_filename, \
     clean_old_files, match_prefixes, create_parents, unique_path, \
-    safe_unlink, file_size
-
+    safe_unlink, file_size, post_to_file
 
 """
 The 'rover download' command - download data from a URL (and then call ingest).
@@ -31,10 +30,16 @@ class Downloader(SqliteSupport):
 
     rover download url [path]
 
-Download a single request (typically for a day) to the given path, ingest and index it.  If no path is given then
-a temporary file is created and deleted after use.
+    rover download file [path]
+
+If given a URL, download a single request (typically for a day) to the given path, ingest and index it.  If no path
+is given then a temporary file is created and deleted after use.
 
 The url should be for a Data Select service, and should not request data that spans multiple calendar days.
+
+If given a file name, POST that file to URL given by the `dataselect-url` configuration parameter, download the
+response to the given path,  ingest and index it.  If no path is given then a temporary file is created and deleted
+after use.
 
 This task is the main low-level task called in the processing pipeline (it calls ingest and index as needed).
 Because of this, to reduce the quantity of unhelpful logs generated when a pipeline is running, empty logs are
@@ -42,6 +47,7 @@ automatically deleted on exit.
 
 ##### Significant Parameters
 
+@dataselect-url
 @temp-dir
 @http-timeout
 @http-retries
@@ -60,7 +66,11 @@ commands for more details.
     rover download \\
     'http://service.iris.edu/fdsnws/dataselect/1/query?net=IU&sta=ANMO&loc=00&cha=BHZ&start=2010-02-27T06:30:00.000&end=2010-02-27T10:30:00.000'
 
-will download, ingest and index data from the given URL..
+will download, ingest and index data from the given URL.
+
+    rover download myrequest.txt
+
+will download, ingest and index data from `dataselect-url` after POSTing `myrequest.txt`.
 
 """
 
@@ -78,6 +88,7 @@ will download, ingest and index data from the given URL..
     def __init__(self, config):
         SqliteSupport.__init__(self, config)
         self._temp_dir = config.dir(TEMPDIR)
+        self._dataselect_url = config.arg(DATASELECTURL)
         self._delete_files = config.arg(DELETEFILES)
         self._blocksize = 1024 * 1024
         self._ingest = config.arg(INGEST)
@@ -88,43 +99,54 @@ will download, ingest and index data from the given URL..
 
     def run(self, args):
         """
-        Download the give URL, then call ingest and index before deleting.
+        Download the give URL or file, then call ingest and index before deleting.
         """
         if len(args) < 1 or len(args) > 2:
             raise Exception('Usage: rover %s url [path]' % DOWNLOAD)
-        url = args[0]
-        if len(args) == 2:
-            path, delete = args[2], False
+        in_path_or_url = args[0]
+        db_path = self._ingesters_db_path(in_path_or_url)
+        if '://' in in_path_or_url:
+            url, in_path, get = in_path_or_url, None, True
+            if '&' not in url:
+                self._log.warn(('The URL provided is "%s" - this does not contain any ampersands.  ' +
+                                'You may have forgotten to quote the URL (and should expect failure).') % url)
         else:
-            path, delete = unique_path(self._temp_dir, TMPDOWNLOAD, url), True
-        if '&' not in url:
-            self._log.warn(('The URL provided is "%s" - this does not contain any ampersands.  ' +
-                            'You may have forgotten to quote the URL (and should expect failure).') % url)
-        db_path = self._ingesters_db_path(url, getpid())
+            url, in_path, get = self._dataselect_url, in_path_or_url, False
+            if not exists(in_path):
+                raise Exception('Could not find file "%s"' % in_path)
+        if len(args) == 2:
+            out_path, delete_out = args[2], False
+        else:
+            out_path, delete_out = unique_path(self._temp_dir, TMPDOWNLOAD, in_path_or_url), True
         try:
-            if self._do_download(url, path):  # False when no data available
+            if self._do_download(get, url, in_path, out_path):  # False when no data available
                 if self._ingest:
-                    Ingester(self._config).run([path], db_path=db_path)
+                    Ingester(self._config).run([out_path], db_path=db_path)
         finally:
             if self._delete_files:
-                if delete:
-                    safe_unlink(path)
+                if delete_out:
+                    safe_unlink(out_path)
                 log_path = self._config.log_path
                 # avoid lots of empty logs cluttering things up
                 if exists(log_path) and file_size(log_path) == 0:
                     safe_unlink(log_path)
                 safe_unlink(db_path)
 
-    def _do_download(self, url, path):
+    def _do_download(self, get, url, in_path, out_path):
         # previously we extracted the file name from the header, but the code
         # failed in python 2 (looked like a backport library bug), so now we let the user specify,
-        if exists(path):
-            raise Exception('Path %s for download already exists' % path)
-        create_parents(path)
-        response, check_status = get_to_file(url, path, self._http_timeout, self._http_retries, self._log)
+        if exists(out_path):
+            raise Exception('Path %s for download already exists' % out_path)
+        create_parents(out_path)
+        if get:
+            response, check_status = get_to_file(url, out_path,
+                                                 self._http_timeout, self._http_retries, self._log)
+        else:
+            response, check_status = post_to_file(url, in_path, out_path,
+                                                  self._http_timeout, self._http_retries, self._log)
         check_status()
         return response
 
-    def _ingesters_db_path(self, url, pid):
+    def _ingesters_db_path(self, url):
         name = uniqueish('rover_ingester', url)
         return unique_filename(join(self._temp_dir, name))
